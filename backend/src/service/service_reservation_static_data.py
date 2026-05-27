df9861469ffd1ca902c5a4af3bd168a1601d55f0
Hylson <hvescovi@gmail.com>
2026-05-26 23:10:21 -0300

    feat:create and get reservations service, with unit tests

---
+++ backend/src/service/service_reservation_static_data.py
@@ -0,0 +1,38 @@
+import datetime
+
+# static data, for testing purposes
+joao_data = {
+            "user": "Joao da Silva",
+            "artifact": "Sala C08", 
+            "date": datetime.date(2026, 5, 26),
+            "start_time": datetime.time(14, 0),
+            "end_time": datetime.time(15, 0),
+            "purpose":"Aula Info 302 - Optativa Web / Hylson",
+            "category":"Sala de Aula",
+            "color_category":"#574DA5",
+            "active":True,
+            "batch_id":None,
+            "observations":None,
+            "created_at":datetime.datetime(2026, 5, 26, 13, 0),
+            "canceler_user":None,
+            "observers":None,
+            "id":1
+        }
+
+maria_data = {
+            "user": "Maria Oliveira",
+            "artifact": "Sala C09",
+            "date": datetime.date(2026, 5, 27),
+            "start_time": datetime.time(10, 0),
+            "end_time": datetime.time(11, 0),
+            "purpose":"Reunião de equipe",
+            "category":"Sala de Reunião",
+            "color_category":"#FF5733",
+            "active":True,
+            "batch_id":None,
+            "observations":None,
+            "created_at":datetime.datetime(2026, 5, 27, 9, 0),
+            "canceler_user":None,
+            "observers":None,
+            "id":2
+        }
