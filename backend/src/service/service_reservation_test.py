262718f9d86a358edcac1f5d0147ab8c7f3a3515
Hylson <hvescovi@gmail.com>
2026-06-05 19:05:34 -0300

    fix: CRUD ok, tests ok

--- backend/src/service/service_reservation_test.py
+++ backend/src/service/service_reservation_test.py
@@ -57,45 +57,15 @@ class TestServiceReservation(unittest.TestCase):
 
         # testing reservation id 1
         self.assertEqual(get_reservation(1), 
-            {"result":"ok", "details": {
-            "user": "Joao da Silva",
-            "artifact": "Sala C08", 
-            "date": datetime.date(2026, 5, 26),
-            "start_time": datetime.time(14, 0),
-            "end_time": datetime.time(15, 0),
-            "purpose":"Aula Info 302 - Optativa Web / Hylson",
-            "category":"Sala de Aula",
-            "color_category":"#574DA5",
-            "active":True,
-            "batch_id":None,
-            "observations":None,
-            "created_at":datetime.datetime(2026, 5, 26, 13, 0),
-            "canceler_user":None,
-            "observers":None,
-            "id":1
-        }})
+            {"result":"ok", "details": joao_data})
         
         # testing reservation id 2
-        self.assertEqual(get_reservation(2), {"result":"ok", "details": {
-            "user": "Maria Oliveira",
-            "artifact": "Sala C09",
-            "date": datetime.date(2026, 5, 27),
-            "start_time": datetime.time(10, 0),
-            "end_time": datetime.time(11, 0),
-            "purpose":"Reunião de equipe",
-            "category":"Sala de Reunião",
-            "color_category":"#FF5733",
-            "active":True,
-            "batch_id":None,
-            "observations":None,
-            "created_at":datetime.datetime(2026, 5, 27, 9, 0),
-            "canceler_user":None,
-            "observers":None,
-            "id":2
-        }})
-
+        self.assertEqual(get_reservation(2), 
+            {"result":"ok", "details": maria_data})
+        
         # testing reservation id that does not exist
-        self.assertEqual(get_reservation(999), {"result":"error", "details":"Reservation not found, id=999"})
+        self.assertEqual(get_reservation(999), 
+                         {"result":"error", "details":"Reservation not found, id=999"})
 
 # - cancellation tests ----------------------------------------
 
